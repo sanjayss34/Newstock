ea8ba8f6c3c33be6c167d8f8c6fa94bafd72efda
jimtse1 <jimtse@seas.upenn.edu>
2016-09-09 23:58:31 -0400

    extract text in dictionary

--- news.py
+++ news.py
@@ -1,8 +1,19 @@
 from py_bing_search import PyBingNewsSearch
+from bs4 import BeautifulSoup
+import urllib
 
 def get_news_articles(query, limit=30):
 	api_key = 'uMbXJDSBqOpPB696+Ez0s+NcznWvurhuAKUruZuWXTA'
 	bing_news = PyBingNewsSearch(api_key, query)
 	results = bing_news.search(limit=limit, format='json')
 	return results
+
+def get_text(list_of_news_articles):
+	dict_of_text = {}
+	for news_articles in list_of_news_articles:
+		url = news_articles.url
+		r = urllib.urlopen(url).read()
+		soup = BeautifulSoup(r)
+		dict_of_text[url] = soup.title.get_text() + '/n' + soup.body.get_text()
+	return dict_of_text
 
