ad56746bdcb166747511c36570633ce57b389cc7
Sumit <ssumit@seas.upenn.edu>
2016-09-10 01:13:48 -0400

    added sentiment analysis

--- news.py
+++ news.py
@@ -2,7 +2,7 @@ from py_bing_search import PyBingNewsSearch
 from bs4 import BeautifulSoup
 import urllib
 
-def get_news_articles(query, limit=30):
+def get_news_articles(query, limit=5):
 	api_key = 'uMbXJDSBqOpPB696+Ez0s+NcznWvurhuAKUruZuWXTA'
 	bing_news = PyBingNewsSearch(api_key, query)
 	results = bing_news.search(limit=limit, format='json')
@@ -14,6 +14,23 @@ def get_text(list_of_news_articles):
 		url = news_articles.url
 		r = urllib.urlopen(url).read()
 		soup = BeautifulSoup(r)
-		dict_of_text[url] = soup.title.get_text() + '/n' + soup.body.get_text()
+		# kill all script and style elements
+		for script in soup(["script", "style"]):
+		    script.extract()    # rip it out
+
+		# get text
+		text = soup.get_text()
+
+		# break into lines and remove leading and trailing space on each
+		lines = (line.strip() for line in text.splitlines())
+		# break multi-headlines into a line each
+		chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+		# drop blank lines
+		text = '\n'.join(chunk for chunk in chunks if chunk)
+
+		dict_of_text[url] = text
+
 	return dict_of_text
 
+def get_data(query):
+	return get_text(get_news_articles('apple'))
