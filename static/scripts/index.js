e192771391924af9420931eeb63ddd7ef8f8eb13
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2016-09-10 01:15:47 -0400

    Basic front-end

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,7 +1,43 @@
-var InitialComponent = React.createClass({
+var NavBar = React.createClass({
     render: function() {
-        return React.createElement('div', {}, 'Hello, world');
+        return React.createElement('nav', {className: "navbar navbar-default"},
+            React.createElement('div', {className: 'container-fluid'},
+                React.createElement('div', {className: 'navbar-header'},
+                    React.createElement('a', {className: 'navbar-brand', href: "#"}, "Newstock"))));
     }
 });
 
-ReactDOM.render(React.createElement(InitialComponent), document.getElementById('body'));
+var InputForm = React.createClass({
+    render: function() {
+        return React.createElement('form', {className: 'form-inline'},
+            React.createElement('div',
+                {className: "input-group",
+                 style: {width: "100%", textAlign: 'center'}
+                },
+                React.createElement('div',
+                {className: 'col-xs-15',
+                 style: {display: 'inline-block'}
+                },
+                React.createElement("input",
+                {type: "text",
+                className: "form-control",
+                id: "symbol",
+                style: {borderRadius: "10px 0px 0px 10px"}
+                }),
+                React.createElement("button",
+                {type: "submit",
+                 className: "btn btn-default",
+                 style: {borderRadius: "0px 10px 10px 0px"}
+                }, 'Submit'))));
+    }
+});
+
+var App = React.createClass({
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement(NavBar),
+            React.createElement(InputForm));
+    }
+});
+
+ReactDOM.render(React.createElement(App), document.getElementById('body'));
