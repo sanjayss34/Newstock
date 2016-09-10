14979c659ffdb554a4e5acae6bec391d2449431d
Sumit <ssumit@seas.upenn.edu>
2016-09-09 23:28:54 -0400

    updated query code

--- news.py
+++ news.py
@@ -1,7 +1,8 @@
 from py_bing_search import PyBingNewsSearch
 
-def get_news_articles(query, limit=10):
+def get_news_articles(query, limit=30):
 	api_key = 'uMbXJDSBqOpPB696+Ez0s+NcznWvurhuAKUruZuWXTA'
 	bing_news = PyBingNewsSearch(api_key, query)
 	results = bing_news.search(limit=limit, format='json')
 	return results
+
